232f5889ba89f251fd296fbfa064cf786afbea65
Rohit kumar <askrohiitsharma@gmail.com>
2026-09-04 18:54:55 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 704166-1-to-n-without-loops-GFG/704166-1-to-n-without-loops.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void printTillN(int n) {
+        // code here
+        for(int i = 1; i<=n;i++){
+            System.out.print(i+" ");
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
