c25fb5e026077897b1fbde0b5395208af0348d54
Rohit kumar <askrohiitsharma@gmail.com>
2026-09-09 23:24:24 +0530

    Time: 6 ms (28.47%), Space: 44.6 MB (86.62%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Deque<String> stk = new ArrayDeque<>();
+        for (String s : path.split("/")) {
+            if ("".equals(s) || ".".equals(s)) {
+                continue;
+            }
+            if ("..".equals(s)) {
+                stk.pollLast();
+            } else {
+                stk.offerLast(s);
+            }
+        }
+        return "/" + String.join("/", stk);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
