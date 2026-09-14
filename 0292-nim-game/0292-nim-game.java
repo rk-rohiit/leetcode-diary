78382c9ad152fead912b599214dffc8118bcd7c3
Rohit kumar <askrohiitsharma@gmail.com>
2026-09-14 11:34:13 +0530

    Time: 0 ms (100%), Space: 42.1 MB (34.76%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 0292-nim-game/0292-nim-game.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean canWinNim(int n) {
+        return n%4 != 0;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
