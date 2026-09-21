746ac79925ba24f4dd613bca7238f2332b5922e4
Rohit kumar <askrohiitsharma@gmail.com>
2026-09-22 01:26:44 +0530

    Time: 14 ms (67.31%), Space: 91.8 MB (96.15%) - LeetHub - Time: O(n * k), Space: O(k)

---
+++ 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.java
@@ -0,0 +1,27 @@
+class Solution {
+    public long[] resultArray(int[] nums, int k) {
+        long[] ans = new long[k];
+   
+    long[] dp = new long[k];
+
+    for (final int num : nums) {
+      long[] newDp = new long[k];
+      final int numMod = num % k;
+   
+      newDp[numMod] = 1;
+      for (int i = 0; i < k; ++i) {
+        final int newMod = (int) (1L * i * numMod % k);
+        newDp[newMod] += dp[i];
+      }
+      for (int i = 0; i < k; ++i)
+        ans[i] += newDp[i];
+      dp = newDp;
+    }
+
+    return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
