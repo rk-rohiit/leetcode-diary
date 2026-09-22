9ac84cd298c74452cc9f3857558ce992cb2f0139
Rohit kumar <askrohiitsharma@gmail.com>
2026-09-23 00:33:11 +0530

    Time: 7 ms (58.58%), Space: 20.8 MB (8.13%) - LeetHub - Time: O(mn), Space: O(mn)

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        @cache
+        def dfs(i, j):
+            if j >= n:
+                return i == m
+            if j + 1 < n and p[j + 1] == '*':
+                return dfs(i, j + 2) or (
+                    i < m and (s[i] == p[j] or p[j] == '.') and dfs(i + 1, j)
+                )
+            return i < m and (s[i] == p[j] or p[j] == '.') and dfs(i + 1, j + 1)
+
+        m, n = len(s), len(p)
+        return dfs(0, 0)
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
